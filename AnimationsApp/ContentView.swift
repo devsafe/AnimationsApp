297d16db43e9d727c9d35e5835730a5dda0af1d5
Rafinad <rafinad@outlook.com>
2021-05-08 01:24:04 +0300

    spring animation exercise

--- AnimationsApp/ContentView.swift
+++ AnimationsApp/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var scale: CGFloat = 1
+    @State private var borderThickness: CGFloat = 1
+    @State private var angle: CGFloat = 1
     var body: some View {
         Button(action: {
-            self.scale += 1
+            //self.scale += 1
+            self.angle += 90
+           // self.borderThickness += 1
         }) {
             Text("Tap Me")
+                .padding()
+                .border(Color.pink, width: borderThickness)
+                .rotationEffect(.degrees(Double(angle)))
+                // .scaleEffect(angle)
+                //.animation(.easeOut(duration: 1))
+                .animation(.interpolatingSpring(mass: 1, stiffness: 1, damping: 0.5, initialVelocity: 5))
         }
-            .padding()
+        .padding()
     }
 }
 

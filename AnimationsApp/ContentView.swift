a3932dbe84b96386188508f7d26c77fa29c8f881
Rafinad <rafinad@outlook.com>
2021-05-08 01:44:47 +0300

    animation onappear

--- AnimationsApp/ContentView.swift
+++ AnimationsApp/ContentView.swift
@@ -8,24 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingWelcome = false
     @State private var scale: CGFloat = 1
     @State private var borderThickness: CGFloat = 1
     @State private var angle: CGFloat = 1
+    @State private var ani: CGFloat = 1
     var body: some View {
-        Button(action: {
-            //self.scale += 1
-            self.angle += 90
-           // self.borderThickness += 1
-        }) {
-            Text("Tap Me")
-                .padding()
-                .border(Color.pink, width: borderThickness)
-                .rotationEffect(.degrees(Double(angle)))
-                // .scaleEffect(angle)
-                //.animation(.easeOut(duration: 1))
-                .animation(.interpolatingSpring(mass: 1, stiffness: 1, damping: 0.5, initialVelocity: 5))
+        VStack {
+            Rectangle()
+                .fill(Color.pink)
+                .frame(width: 200, height: 200)
+                .scaleEffect(scale)
+                .onAppear() {
+                    let baseAnimation = Animation.easeInOut(duration: 1)
+                    let repeated = baseAnimation.repeatForever(autoreverses: true)
+                    return withAnimation(repeated) {
+                        self.scale = 0.5
+                    }
+                }
+            Button(action: {
+                //self.scale += 1
+                self.angle += 90
+               // self.borderThickness += 1
+            }) {
+                Text("Tap Me")
+                    .padding()
+                    .border(Color.pink, width: borderThickness)
+                    .rotationEffect(.degrees(Double(angle)))
+                    // .scaleEffect(angle)
+                    //.animation(.easeOut(duration: 1))
+                    .animation(.interpolatingSpring(mass: 1, stiffness: 1, damping: 0.5, initialVelocity: 5))
+            }
+            .padding()
+            Toggle(isOn: $showingWelcome.animation(.spring()), label: {
+                Text("Label")
+            })
+            if showingWelcome {
+                Text("Hello SwiftUI!")
+            }
         }
-        .padding()
     }
 }
 

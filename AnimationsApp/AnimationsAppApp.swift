f63df82a0a14a977a481ce99ba3502805f4c6b43
Rafinad <rafinad@outlook.com>
2021-05-08 01:04:50 +0300

    Initial Commit

---
+++ AnimationsApp/AnimationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsAppApp.swift
+//  AnimationsApp
+//
+//  Created by Сахар Кубический on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
